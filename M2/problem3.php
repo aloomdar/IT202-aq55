0b668e3020744b2200e05eb43d29d3ecd573180a
aq55 <aq55@njit.edu>
2024-02-18 13:42:16 -0500

    completed hw

--- M2/problem3.php
+++ M2/problem3.php
@@ -4,6 +4,7 @@ $a2 = [-1, 1, -2, 2, 3, -3, -4, 5];
 $a3 = [-0.01, -0.0001, -.15];
 $a4 = ["-1", "2", "-3", "4", "-5", "5", "-6", "6", "-7", "7"];
 
+//aq55
 function bePositive($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>" ;
     echo "<br>Positive output:<br>";
