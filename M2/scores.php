9579701f81d157417064e2da977d540b2b9d8b50
aloomdar <aqanoongo@gmail.com>
2024-04-19 14:07:20 -0400

    fixed issues displaying data

--- M2/scores.php
+++ M2/scores.php
@@ -28,7 +28,7 @@ AND date < curdate() - INTERVAL DAYOFWEEK(curdate())-1 DAY
 */
 function weekly(){
     $db = getDB();
-    $stmt = $db->prepare("SELECT score, user_id, modified FROM Scores WHERE modified >= CURDATE - INTERVAL DAYOFWEEK(curdate())+6 DAY AND date < CURDATE() - INTERVAL DAYOFWEEK(CURDATE())-1 DAY LIMIT 10");
+    $stmt = $db->prepare("SELECT score, user_id, modified FROM Scores WHERE modified >= CURDATE() - INTERVAL DAYOFWEEK(curdate())+6 DAY AND modified < CURDATE() - INTERVAL DAYOFWEEK(CURDATE())-1 DAY LIMIT 10");
     try{
         $r = $stmt->execute();
         if($r){
@@ -36,7 +36,7 @@ function weekly(){
       
             foreach($scores as $score){
                 echo '<tr>'; 
-                echo '<td>' . $score['username'] . '</td>'; 
+                echo '<td>' . $score['user_id'] . '</td>'; 
                 echo '<td>' . $score['score'] . '</td>'; 
                 echo '<td>' . $score['modified'] . '</td>'; 
                 echo '</tr>';
@@ -53,7 +53,7 @@ function weekly(){
 */
 function monthly(){
     $db = getDB();
-    $stmt = $db->prepare("SELECT score, user_id, modified FROM Scores WHERE MONTH(modified) = MONTH(GETDATE()) LIMIT 10");
+    $stmt = $db->prepare("SELECT score, user_id, modified FROM Scores WHERE MONTH(modified) = MONTH(CURDATE()) LIMIT 10");
     try{
         $r = $stmt->execute();
         if($r){
@@ -61,7 +61,7 @@ function monthly(){
       
             foreach($scores as $score){
                 echo '<tr>'; 
-                echo '<td>' . $score['username'] . '</td>'; 
+                echo '<td>' . $score['user_id'] . '</td>'; 
                 echo '<td>' . $score['score'] . '</td>'; 
                 echo '<td>' . $score['modified'] . '</td>'; 
                 echo '</tr>';
