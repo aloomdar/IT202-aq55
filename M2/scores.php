8b526e348aa3ab15c47ccb5b8a0b53f936c95f42
aloomdar <aqanoongo@gmail.com>
2024-04-19 13:45:38 -0400

    added scores function

---
+++ M2/scores.php
@@ -0,0 +1,74 @@
+<?php
+function daily(){
+    $db = getDB();
+    $stmt = $db->prepare("SELECT score, user_id, modified FROM Scores WHERE DATE(modified) = CURDATE() LIMIT 10");
+    try{
+        $r = $stmt->execute();
+        if($r){
+            $scores = $stmt->fetchALL(PDO::FETCH_ASSOC); 
+      
+            foreach($scores as $score){
+                echo '<tr>'; 
+                echo '<td>' . $score['user_id'] . '</td>'; 
+                echo '<td>' . $score['score'] . '</td>'; 
+                echo '<td>' . $score['modified'] . '</td>'; 
+                echo '</tr>';
+            }
+        }
+    }
+    catch(Exception $e){
+        echo var_export($e);
+    }
+}
+
+
+/*SELECT id FROM tbl
+WHERE date >= curdate() - INTERVAL DAYOFWEEK(curdate())+6 DAY
+AND date < curdate() - INTERVAL DAYOFWEEK(curdate())-1 DAY
+*/
+function weekly(){
+    $db = getDB();
+    $stmt = $db->prepare("SELECT score, user_id, modified FROM Scores WHERE modified >= CURDATE - INTERVAL DAYOFWEEK(curdate())+6 DAY AND date < CURDATE() - INTERVAL DAYOFWEEK(CURDATE())-1 DAY LIMIT 10");
+    try{
+        $r = $stmt->execute();
+        if($r){
+            $scores = $stmt->fetchALL(PDO::FETCH_ASSOC); 
+      
+            foreach($scores as $score){
+                echo '<tr>'; 
+                echo '<td>' . $score['username'] . '</td>'; 
+                echo '<td>' . $score['score'] . '</td>'; 
+                echo '<td>' . $score['modified'] . '</td>'; 
+                echo '</tr>';
+            }
+        }
+    }
+    catch(Exception $e){
+        echo var_export($e);
+    }
+}
+
+/*
+
+*/
+function monthly(){
+    $db = getDB();
+    $stmt = $db->prepare("SELECT score, user_id, modified FROM Scores WHERE MONTH(modified) = MONTH(GETDATE()) LIMIT 10");
+    try{
+        $r = $stmt->execute();
+        if($r){
+            $scores = $stmt->fetchALL(PDO::FETCH_ASSOC); 
+      
+            foreach($scores as $score){
+                echo '<tr>'; 
+                echo '<td>' . $score['username'] . '</td>'; 
+                echo '<td>' . $score['score'] . '</td>'; 
+                echo '<td>' . $score['modified'] . '</td>'; 
+                echo '</tr>';
+            }
+        }
+    }
+    catch(Exception $e){
+        echo var_export($e);
+    }
+}
