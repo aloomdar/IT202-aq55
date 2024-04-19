192ee5e766573b67dbece351d237fa98e21ec0ca
aloomdar <aqanoongo@gmail.com>
2024-04-19 13:46:05 -0400

    modified homepage

--- M2/index.php
+++ M2/index.php
@@ -1,1 +1,44 @@
+<?php
+require (__DIR__ . "/scores.php");
+require (__DIR__ . "/partials/nav.php");
+?>
+
 <?php echo "It works!";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to the Home Page</title>
+</head>
+<body>
+    <div class="main">
+        <table>
+            <h1>Daily Scores</h1>
+            <tr>
+                <td>Player</td>
+                <td>Score</td>
+                <td>Date</td>
+            </tr>
+            <tr>
+                <?php
+                daily();
+                ?>
+            </tr>
+        </table>
+        <table>
+            <h1>Weekly Scores</h1>
+            <tr>
+                <td>Player</td>
+                <td>Score</td>
+                <td>Date</td>
+            </tr>
+            <tr>
+                <?php
+                weekly();
+                ?>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
